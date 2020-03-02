2ed5d9f7fb1364c63448b1df3393d1bb426319a9
Fenris <anatolijv236@gmail.com>
2020-03-02 23:15:56 +0300

    hamburger

--- js/scripts.js
+++ js/scripts.js
@@ -34,4 +34,12 @@ $(document).ready(() => {
             }
         }]
     });
+
+    /**
+     * HAMBURGER
+     */
+    $('.menu-toggle-inner').on('click', function (e) {
+        $('.adaptive-menu-toggle').toggleClass('adaptive-menu-toggle--open');
+        $('.headerNav_adaptive').toggleClass('header_opened');
+    });
 });
